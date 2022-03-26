4a82961e99e3363993395b8763f009cf4498535f
PinkLemon <paulgan98@gmail.com>
2022-03-25 18:19:45 -0700

    Updates

---
+++ precache-manifest.8bbeebc7064a56bef73d323a17eaf042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55aa83c0353caa13ac8d",
+    "url": "/prime-spiral/static/css/main.2a8e11b1.chunk.css"
+  },
+  {
+    "revision": "55aa83c0353caa13ac8d",
+    "url": "/prime-spiral/static/js/main.55aa83c0.chunk.js"
+  },
+  {
+    "revision": "a2eba8c93c54baa19bb9",
+    "url": "/prime-spiral/static/js/1.a2eba8c9.chunk.js"
+  },
+  {
+    "revision": "3f8625f0b3ac0b4f5781",
+    "url": "/prime-spiral/static/js/runtime~main.3f8625f0.js"
+  },
+  {
+    "revision": "cac0b5d1f09b59627a284b5e63dcbd95",
+    "url": "/prime-spiral/index.html"
+  }
+];
